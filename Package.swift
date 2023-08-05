70667d7ce98d7222b7171f0539eb666405a384b7
Stephan Casas <stephancasas@icloud.com>
2023-08-04 20:16:48 -0500

    add platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SymbolicBridge",
+    platforms: [.macOS(.v13)],
     products: [
         .library(
             name: "SymbolicBridge",
